9c6425d272be8fefa1d06bbae1f55ea0fb39108f
Cristhian García Vélez <escipion25@gmail.com>
2021-08-06 11:26:28 -0500

    Clase 12

--- posts/models.py
+++ posts/models.py
@@ -9,9 +9,14 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank=True)
 
     birthday = models.DateField(blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
