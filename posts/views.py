9c4d20c1412c6a6f1599a876a04ff511b87c2ec8
Cristhian García Vélez <escipion25@gmail.com>
2021-08-05 20:07:03 -0500

    Clase 8

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
 
 posts = [
     {
-        'tittle': 'Mont Blanc',
-        'user': {
-            'name': 'Yésica Cortés',
-            'picture': 'https://picsum.photos/60/60?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600?image=1036',
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
     },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
 ]
 
 def list_posts(request):
-    return render(request, 'feed.html', { 'posts': posts })
+    """List existing posts."""
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
