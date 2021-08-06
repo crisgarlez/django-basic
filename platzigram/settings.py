c676bdf4641898e5c41b7c6e0017193a08c62931
Cristhian García Vélez <escipion25@gmail.com>
2021-08-05 22:03:33 -0500

    Clase 11

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangobasic',
+        'USER': 'skypiea',
+        'PASSWORD': 'skypiea',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    bio = models.TextField(blank=True)
+
+    birthday = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
