ca44a0163cad4d6459131afa779982d214d414ad
Cristhian García Vélez <escipion25@gmail.com>
2021-08-07 12:07:27 -0500

    Clase 16

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from users.models import Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'phone_number', 'web_site')
+    list_display_links = ('id','user')
+    search_fields = ('phone_number', 'user__username')
+    list_filter = ('created', 'modified', 'user__is_active')
