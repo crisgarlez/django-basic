6f208b1d17aa549f1b2fcb0a062fa10dec6b400b
Cristhian García Vélez <escipion25@gmail.com>
2021-08-15 23:26:04 -0500

    Clase 18

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from platzigram import views
 from posts import views as posts_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-world', views.hello_world),
     path('sorted_integers', views.sorted_integers),
     path('hi/<str:name>/<int:age>', views.hi),
     path('posts/', posts_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
