0cfaf6f9f803722344d8b943f70fdc7eaea8f438
Cristhian García Vélez <escipion25@gmail.com>
2021-08-22 16:51:44 -0500

    Clase 31

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path(
+        route='login/',
+        view=views.LoginView.as_view(),
+        name='login'
+    ),
+
+    path(
+        route='logout/',
+        view=views.LogoutView.as_view(),
+        name='logout'
+    ),
 
     path(
         route='signup/',
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from users.forms import SignupForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -28,27 +29,15 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         username = self.object.user.username
         return reverse('users:detail', kwargs={'username': username})
 
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('feed')
+class LoginView(auth_views.LoginView):
+    """Login view."""
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
+    template_name = 'users/login.html'
 
-    return render(request, 'users/login.html')
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
 
-@login_required
-def logout_view(request):
-
-    logout(request)
-    return redirect('users:login')
+    template_name = 'users/logged_out.html'
 
 class SignupView(FormView):
     """Users sign up view."""
